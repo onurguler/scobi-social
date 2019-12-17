a452961136b56a8e954a0036dd067453cb65dd05
Onur Güler <guleronurosman@gmail.com>
2019-12-17 16:14:13 +0300

    passport jwt auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,6 @@ const router = express.Router();
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => res.send('Auth route'));
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => res.send(req.user));
 
 module.exports = router;
