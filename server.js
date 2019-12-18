56979fc32e5fb9a1b1b43bb0a4a2f8cc818cbbc2
Onur Güler <guleronurosman@gmail.com>
2019-12-19 01:49:28 +0300

    deploy heroku

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running...' + req.user));
-
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
