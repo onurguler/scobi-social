ca40f4d0b5c3a0641b374763b1da881ec66aeb1e
Onur Güler <guleronurosman@gmail.com>
2019-12-18 20:28:24 +0300

    todo username register

--- routes/api/users.js
+++ routes/api/users.js
@@ -12,12 +12,14 @@ const User = require('../../models/User');
 // @route   POST api/users
 // @desc    Register user
 // @access  Public
+// @TODO: username register
 router.post(
   '/',
   [
     check('name', 'Name is required')
       .not()
       .isEmpty(),
+    // check('username', 'Username is required').exists(),
     check('email', 'Please enter a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
   ],
@@ -28,7 +30,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, email, password } = req.body;
+    const { name, username, email, password } = req.body;
 
     try {
       let user = await User.findOne({ email });
@@ -36,6 +38,12 @@ router.post(
       if (user) {
         return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
       }
+
+      // user = await User.findOne({ username });
+
+      // if (user) {
+      //   return res.status(400).json({ errors: [{ msg: 'Username already exists' }] });
+      // }
 
       const avatar = gravatar.url(email, { s: '200', r: 'pg', d: 'mm' });
 
